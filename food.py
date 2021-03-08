f328447672c0fd1e9fd1d512b4e0bf834a372373
Amity01 <68575399+Amity01@users.noreply.github.com>
2021-03-08 13:20:33 +0530

    Create food.py

---
+++ food.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("circle")
+        self.color("blue")
+        self.speed("fastest")
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        x_axis = random.randint(-280, 280)
+        y_axis = random.randint(-280, 280)
+        self.goto(x_axis, y_axis)
+        self.refresh()
+
+    def refresh(self):
+        x_axis = random.randint(-280, 280)
+        y_axis = random.randint(-280, 280)
+        self.goto(x_axis, y_axis)
